7861d91fedb5eb0b7b80fae08825033886f67b1d
Crislp12 <cristopher.picopinos@gmail.com>
2022-03-23 21:49:02 +0000

    third

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function multiplesOfThree() {
         }
     }
 } multiplesOfThree(); 
+
+function multiplesOfThreeOrFive () {
+    console.log("---q3--")
+    for (let i =0; i < 100; i++){
+        if ( i % 3 ===0 && i % 5 === 0){
+            console.log(i)
+        }
+    }
+    
+    
+}
+ multiplesOfThreeOrFive()
